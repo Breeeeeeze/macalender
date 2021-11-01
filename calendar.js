cb39580164daa8f56c068ca823dc1ce3f1631250
Glitch (macalendar2) <none>
2021-11-01 19:15:01 +0000

    ⛺️💍 Checkpoint ./calendar.js:36567841/371

--- calendar.js
+++ calendar.js
@@ -1,6 +1,6 @@
 /*https://code-boxx.com/simple-pure-javascript-calendar-events/*/
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-analytics.js";
+import { initializeApp } from 'firebase/app';
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,7 +18,7 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
+  const db = getFirestore(app);
 
 var cal = {
   // (A) PROPERTIES
